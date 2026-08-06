540966dc94cc61973bf4c70957201d529810b4a8
elnur549 <elnurtaken@gmail.com>
2026-08-06 14:59:58 +0400

    Add product API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use Illuminate\Routing\Route;
+use App\Http\Controllers\api\ProductController;
+// use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products',ProductController::class);
